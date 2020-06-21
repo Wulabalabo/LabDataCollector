a29b82ef14f18cab34001e6a11d47d2e4d187910
JasonChen <138670424@qq.com>
2020-06-22 01:40:17 +0800

    Initial

---
+++ LabDataCollector/Assets/LabDataCollector/Demo/LabResultDemoData1.cs
@@ -0,0 +1,16 @@
+﻿using DataSync;
+
+namespace LabDataCollector.Demo
+{
+    internal class LabResultDemoData1 : LabDataBase
+    {
+        public string v1 { get; set; }
+        public string v2 { get; set; }
+
+        public LabResultDemoData1(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+}
---
+++ LabDataCollector/Assets/LabDataCollector/Scripts/Utils/LabExtension.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Reflection;
+
+namespace LabDataCollector
+{
+    public static class LabExtension 
+    {
+        public static string ToJson(this object o)
+        {
+            return JsonConvert.SerializeObject(o, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                NullValueHandling = NullValueHandling.Include
+                
+            });           
+        }
+
+        public static Pos ToPos(this Vector3 vector3)
+        {
+            return new Pos()
+            {
+                X = vector3.x,
+                Y = vector3.y,
+                Z = vector3.z
+            };
+        }
+        public static QuaternionPos ToQuaternionPos(this Quaternion quaternion)
+        {
+            return new QuaternionPos()
+            {
+                X = quaternion.x,
+                Y = quaternion.y,
+                Z = quaternion.z,
+                W = quaternion.w
+            };
+        }
+
+      
+    }
+
+}
